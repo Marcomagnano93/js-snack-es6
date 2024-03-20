2f6fad62f2b0b832dd81090f28cec3fedf26aefe
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-20 15:48:18 +0100

    ???

--- js/app.js
+++ js/app.js
@@ -60,11 +60,9 @@ console.log(segnaPostListMap)
 function studentCardGenerator (idNumber, studentName, studentGrades){
 
     const student = {
-
         id: idNumber,
         name: studentName,
         grade: studentGrades
-
     }
 
     return student
@@ -78,7 +76,8 @@ const students = [
     studentCardGenerator(145, "Gaia Borromini", 74),
     studentCardGenerator(196, "Luigi Grimaldello", 68),
     studentCardGenerator(102, "Piero della Francesca", 50),
-    studentCardGenerator(120, "Francesca da Polenta", 84)]; //objectArray
+    studentCardGenerator(120, "Francesca da Polenta", 84)
+    ]; //objectArray
 
 console.log(students)
 
@@ -102,18 +101,151 @@ console.log(studentsGradesFiltered);
 const studentsGradesAndIdFiltered = studentsGradesFiltered.filter((student) => student.id > 120); //object array
 console.log(studentsGradesAndIdFiltered);
 
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+// SNACK 3
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+function bikeGenerator (type, bicicleWeight){
+
+    const bike = {
+        name: type,
+        weight: bicicleWeight
+    }
+    return bike
+}
+
+const bicicles = [
+    bikeGenerator("forte", 30),
+    bikeGenerator("leggera", 12),
+    bikeGenerator("sportiva", 25),
+    bikeGenerator("elegante", 22)
+]
+
+console.log(bicicles)
+
+const bikeswh = []
+
+for(let x = 0; x < bicicles.length; x++){
+
+    const xIndex = bicicles[x];
+
+    bikeswh.push(xIndex.weight)
+}
+
+const lowestWeight = Math.min(...bikeswh)
+
+
+let lightBike = bicicles[x].weight === lowestWeight ? ` è la bici più leggera` : ` non è la più leggera`
+    console.log(lightBike)
+
+
+console.log(bikeswh)
+console.log(lowestWeight)
 
 
 
 
 
 
+// const bike1 = {
+//     name: "sport",
+//     weight: 30
+// }
+const bike1 = {
+    name: "elegant",
+    weight: 25
+}
+
+
+const bike2 = {
+    name: "elegant",
+    weight: 25
+}
+
+const bike3 = {
+    name: "wow",
+    weight: 10
+}
+
+const bikes = [bike1, bike2, bike3];
+
+
+for(let bikeIndex = 0; x < bikes.length; bikeIndex++){
+
+    const ind = bikes[bikeIndex];
+
+    
+}
+
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+// SNACK 4
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+// function footBallSquad (squad, score = 0, fall = 0){
+
+//     const squadObject = {
+//         name: squad,
+//         pti: score,
+//         falls: fall
+//     }
+//     return squadObject
+// }
+
+// function fSquad ({name, pti}){
+//     return name, pti
+// }
+
+
+
+
+// function getRandom1To100() {
+//     randomNumber = Math.floor(Math.random() * 100) + 1;
+//     return randomNumber
+//   }
 
 
 
 
 
+// const squadArray = [
+//     footBallSquad("Roma"),
+//     footBallSquad("Torino"),
+//     footBallSquad("Milan"),
+//     footBallSquad("Inter"),
+//     footBallSquad("Juve"),
+// ]
 
+
+// for(let y = 0; y < squadArray.length; y++){
+
+//     const index = squadArray[y];
+
+//     const newPti = index.pti = getRandom1To100();
+//     const newFalls = index.falls = getRandom1To100();
+// }
+
+
+
+
+
+// console.log(fSquad(squadArray))
+
+
+
+
+
+  
 
 
 
